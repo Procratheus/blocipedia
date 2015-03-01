be445a9cfecec376ada3826f51c9c2ff3a329930
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-03-01 20:26:46 +0000

    x

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 
-  def markdown_to_html
-
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Renderer::HTML.new
+    extensions = { fenced_code_blocks: true}
+    redcarpet = Redcarpet::Mardown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
   end
 
 end
